651d7619d1e6c4c19c3205ab3ab1545b2d866bcc
RASBI <74168235+RASBI-CARDNAME@users.noreply.github.com>
2026-01-22 21:07:30 +0900

    Update DHT11.c

--- Core/Src/DHT11.c
+++ Core/Src/DHT11.c
@@ -24,13 +24,13 @@
 
 #include "dht11.h"
 
-//simple delay_us
+//타이머를 이용한 마이크로초 딜레이 함수
 void delay_us(DHT11_t* dht11, uint16_t time) {
 	__HAL_TIM_SET_COUNTER(dht11->htim,0);
 	while ((uint16_t)__HAL_TIM_GET_COUNTER(dht11->htim) < time);
 }
 
-//set gpio pin mode
+//gpio 핀 초기 설정
 void set_pin_mode(DHT11_t* dht11, uint8_t pinmode) {
 
 	GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -49,20 +49,21 @@ void set_pin_mode(DHT11_t* dht11, uint8_t pinmode) {
 	HAL_GPIO_Init(dht11->GPIO_Channel, &GPIO_InitStruct);
 }
 
-//init DHT11
+//DHT 11 초기화
 void init_DHT11(DHT11_t* dht11) { //User-defined structure values are used for initialization.
 
-	//#0 start timer
+	//#0 타이머 시작
 	HAL_TIM_Base_Start(dht11->htim);
 }
 
+//DHT 11로 부터 정보 읽기
 uint8_t read_DHT11(DHT11_t* dht11, uint8_t* temp_i, uint8_t* temp_d, uint8_t* humidity_i, uint8_t* humidity_d) {
 
-	// #0 variable
+	// #0 변수
 	uint8_t status = 0; //status for check dht11 response
 	uint8_t data[5] = {0};
 
-	// #1 Ask to DHT11
+	// #1 DHT 11 센서 깨우기
 	__disable_irq();
 	set_pin_mode(dht11, DHT11_PINMODE_OUTPUT);
 	HAL_GPIO_WritePin(dht11->GPIO_Channel, dht11->pin, GPIO_PIN_SET);
@@ -75,9 +76,9 @@ uint8_t read_DHT11(DHT11_t* dht11, uint8_t* temp_i, uint8_t* temp_d, uint8_t* hu
 	delay_us(dht11, 30);
 	set_pin_mode(dht11, DHT11_PINMODE_INPUT);
 
-	// #2 Check response - HIGH => 80us LOW => 80us HIGH => LOW, start send data
+	// #2 응답 확인 - HIGH => 80us / LOW => 80us / HIGH => LOW, 데이터 전송 시작
 	delay_us(dht11, 1);
-	// #2-1: Does signal changed to LOW?
+	// #2-1: 신호가 LOW로 떨어지나 확인
 	__HAL_TIM_SET_COUNTER(dht11->htim, 0);
 	while(HAL_GPIO_ReadPin(dht11->GPIO_Channel, dht11->pin)) {
 	    if (__HAL_TIM_GET_COUNTER(dht11->htim) > 50) {
@@ -86,7 +87,7 @@ uint8_t read_DHT11(DHT11_t* dht11, uint8_t* temp_i, uint8_t* temp_d, uint8_t* hu
 	    }
 	}
 
-	// #2-2: Dose signal changed to HIGH?
+	// #2-2: 신호가 다시 HIGH로 올라오나 확인
 	__HAL_TIM_SET_COUNTER(dht11->htim, 0);
 	while(!HAL_GPIO_ReadPin(dht11->GPIO_Channel, dht11->pin)) {
 	    if (__HAL_TIM_GET_COUNTER(dht11->htim) > 90) {
@@ -95,7 +96,7 @@ uint8_t read_DHT11(DHT11_t* dht11, uint8_t* temp_i, uint8_t* temp_d, uint8_t* hu
 	    }
 	}
 
-	// #2-3: Dose signal changed to LOW?
+	// #2-3: 신호가 다시 LOW로 떨어지나 확인
 	__HAL_TIM_SET_COUNTER(dht11->htim, 0);
 	while(HAL_GPIO_ReadPin(dht11->GPIO_Channel, dht11->pin)) {
 	    if (__HAL_TIM_GET_COUNTER(dht11->htim) > 90) {
@@ -104,10 +105,10 @@ uint8_t read_DHT11(DHT11_t* dht11, uint8_t* temp_i, uint8_t* temp_d, uint8_t* hu
 	    }
 	}
 
-	// #3 read data
+	// #3 데이터 읽기
 	for (int i = 0; i<sizeof(data)/sizeof(data[0]); i++) {
 		for (int j = 0; j <8; j++) {
-			//Does signal changed LOW to HIGH?
+			//신호가 LOW -> HIGH로 되었는지 확인
 			__HAL_TIM_SET_COUNTER(dht11->htim,0);
 			while(!HAL_GPIO_ReadPin(dht11->GPIO_Channel, dht11->pin)) {
 				if (__HAL_TIM_GET_COUNTER(dht11->htim)>60) {
@@ -115,7 +116,7 @@ uint8_t read_DHT11(DHT11_t* dht11, uint8_t* temp_i, uint8_t* temp_d, uint8_t* hu
 					goto ERROR_HANDLE;
 				}
 			}
-			//measure HIGH pulse
+			//HIGH 펄스 길이 측정
 			__HAL_TIM_SET_COUNTER(dht11->htim,0);
 			while(HAL_GPIO_ReadPin(dht11->GPIO_Channel, dht11->pin)) {
 				if (__HAL_TIM_GET_COUNTER(dht11->htim)>90) {
@@ -123,19 +124,19 @@ uint8_t read_DHT11(DHT11_t* dht11, uint8_t* temp_i, uint8_t* temp_d, uint8_t* hu
 					goto ERROR_HANDLE;
 				}
 			}
-			//write data
+			//읽은 데이터를 변수에 쓰기
 			if (__HAL_TIM_GET_COUNTER(dht11->htim)>40) {
 				data[i] |= (1<<(7-j));
 			}
 		}
 	}
-	// check check_sum :)
+	// 체크썸 확인 :)
 	if (data[4] != (uint8_t)(data[0]+data[1]+data[2]+data[3])) {
 		status = 6; ////error code 6: check sum error!
 		goto ERROR_HANDLE;
 	}
 
-	// #4 return data
+	// #4 데이터 리턴 
 	*humidity_i = data[0];
 	*humidity_d = data[1];
 	*temp_i = data[2];
